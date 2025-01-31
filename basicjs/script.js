c94ca38a0a9d83ce43aebce2a0c6faecf7a74bad
Absoluteloser <motkulwarbalaji805@gmail.com>
2025-01-31 06:01:58 +0530

    can return while using map

---
+++ basicjs/script.js
@@ -0,0 +1,50 @@
+const x = 1;
+let y = 23;
+console.log(`The value of x is ${x}`);
+console.log(`The value of y is ${y}`);
+y = 'Balaji Nagesh Motkulwar';
+console.log(`Now the value of y is ${y}`);
+// x = 4;
+// the above line will cause error
+
+const t = [1, 2, 3, 4];
+console.log(t);
+console.log(t[1]);
+console.log(`This is something different`);
+t.forEach(function runThis(value) {
+    console.log(value);
+})
+console.log(`The other way is: `);
+t.forEach((val) => {
+    console.log(val);
+})
+const newt = t.map(val => val * 2);
+console.log(newt);
+const newt1 = t.map(val => `<li>` + val + '<li>');
+console.log(newt1);
+
+//map method creates a new array.
+
+let first = [1, 2, 3, 4, 5];
+let second = [4, 5, 6, 7];
+let third = first.concat(second);
+console.log(`viewing third one`);
+console.log(third);
+console.log(`first one is ${first}`);
+//the original array remained unchanged.
+console.log(`This is for concantation`);
+third.forEach(val => {
+    console.log(val);
+})
+
+//concat also creates a new array.
+
+
+console.log(`this is for destructuring`);
+const pq = [3, 4, 5, 6, 67, 8, 4];
+const [f, s, ...tr] = pq;
+console.log(f);
+console.log(s);
+console.log(tr);
+
+//the above one destructuring one
--- exercise1/src/App.jsx
+++ exercise1/src/App.jsx
@@ -1,40 +1,55 @@
+function useThis() {
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
+  return parts.map(({ name, exercises }) => (
+    <Content key={name} part={name} exercises={exercises} />
+  ));
+}
+
 function Header({ course }) {
   return (
     <h2>
       {course}
     </h2>
-  )
+  );
 }
 
 function Content({ part, exercises }) {
   return (
     <p>{part} {exercises}</p>
-  )
+  );
 }
 
 function Total({ e1, e2, e3 }) {
   return (
     <p>Number of exercises {e1 + e2 + e3}</p>
-  )
+  );
 }
 
 function App() {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
   const exercises1 = 10;
-  const part2 = 'Using props to pass data';
   const exercises2 = 7;
-  const part3 = 'State of a component';
   const exercises3 = 14;
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      {useThis()}
       <Total e1={exercises1} e2={exercises2} e3={exercises3} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
