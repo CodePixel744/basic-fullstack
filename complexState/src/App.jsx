6dfaa8f9680e47cd25333411f20fbc4ce8788be5
Absoluteloser <motkulwarbalaji805@gmail.com>
2025-02-03 06:35:21 +0530

    initial setup

---
+++ complexState/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+
+function App() {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>LEFT</button>
+      {right}
+      <button onClick={() => setRight(right + 1)}>RIGHT</button>
+    </div>
+  )
+}
+
+export default App
