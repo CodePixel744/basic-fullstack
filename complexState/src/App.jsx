05ced2960c79e9748fa595af74ff0b01b2230ca5
Absoluteloser <motkulwarbalaji805@gmail.com>
2025-02-03 06:44:26 +0530

    destructuring one

--- complexState/src/App.jsx
+++ complexState/src/App.jsx
@@ -4,11 +4,11 @@ import { useState } from "react"
 function App() {
   const [clicks, setClicks] = useState({ left: 0, right: 0 });
   const handleLeftClick = () => {
-    const newClicks = { left: clicks.left + 1, right: clicks.right };
+    const newClicks = { ...clicks, left: clicks.left + 1 };
     setClicks(newClicks);
   }
   const handleRightClick = () => {
-    const newClicks = { left: clicks.left, right: clicks.right + 1 };
+    const newClicks = { ...clicks, right: clicks.right + 1 };
     setClicks(newClicks);
   }
   return (
