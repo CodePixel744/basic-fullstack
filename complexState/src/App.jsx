581f8028699c8750f8cfad89a18cd6b6ac34aa7d
Absoluteloser <motkulwarbalaji805@gmail.com>
2025-02-03 07:02:04 +0530

    used extra variables to remember the useful state

--- complexState/src/App.jsx
+++ complexState/src/App.jsx
@@ -3,20 +3,39 @@ import { useState } from "react"
 
 function App() {
   const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
   const handleLeftClick = () => {
     const newClicks = { ...clicks, left: clicks.left + 1 };
+    const updatedLeft = clicks.left + 1;
     setClicks(newClicks);
+    /**use .concat instead of .push in React */
+    setAllClicks(allClicks.concat('L'));
+    setTotal(updatedLeft + clicks.right);
   }
   const handleRightClick = () => {
     const newClicks = { ...clicks, right: clicks.right + 1 };
+    const updatedRight = clicks.right + 1;
     setClicks(newClicks);
+    setAllClicks(allClicks.concat('R'));
+    /*because state updates are taking asynchronously. */
+    /**so we used the updated state using new variable. */
+    setTotal(clicks.left + updatedRight);
   }
   return (
     <div>
       {clicks.left}
-      <button onClick={() => handleLeftClick()}>LEFT</button>
+      <button onClick={handleLeftClick}>LEFT</button>
       {clicks.right}
-      <button onClick={() => handleRightClick()}>RIGHT</button>
+      <button onClick={handleRightClick}>RIGHT</button>
+      <div>
+        {allClicks.map((click, index) => (
+          <span key={index}>{click}</span>
+        ))}
+      </div>
+      <p>
+        Total count is {total}.
+      </p>
     </div>
   )
 }
